2e17091ca8fb11755777d0d2070bdfccc8bcdc5a
Zendy <nguyenduc.051194@gmail.com>
2021-08-05 23:09:50 +0700

    feat: setup project

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgoundColor = Color(0xFF091C40);
+const Color kSecondaryColor = Color(0xFF606060);
+const Color kRedColor = Color(0xFFFF1E46);
---
+++ lib/screens/audio_call_width_image/audio_call_with_image_screen.dart
@@ -0,0 +1,13 @@
+import 'package:calling_interface/screens/audio_call_width_image/components/body.dart';
+import 'package:flutter/material.dart';
+
+class AudioCallWithImage extends StatelessWidget {
+  const AudioCallWithImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
